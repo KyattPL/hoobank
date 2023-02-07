534fb7099b880be6bf93f38a490e7b75a7401782
Kajetan Pynka <siema1234@gazeta.pl>
2023-02-07 17:59:57 +0100

    Finished app

--- src/components/Business.tsx
+++ src/components/Business.tsx
@@ -4,6 +4,35 @@ import { features } from '../constants';
 import styles, { layout } from '../style';
 import Button from './Button';
 
+type FeatureCardProps = {
+    icon: string,
+    title: string,
+    content: string,
+    index: number
+};
+
+const FeatureCard = ({ icon, title, content, index }: FeatureCardProps) => {
+    return (
+        <div className={`flex flex-row p-6 rounded-[20px] 
+        feature-card ${index !== features.length - 1 ? 'mb-6' : 'mb-0'}`}>
+            <div className={`w-[64px] h-[64px] rounded-full 
+            ${styles.flexCenter} bg-dimBlue`}>
+                <img src={icon} alt="icon" className='w-1/2 h-1/2 object-contain' />
+            </div>
+            <div className="flex-1 flex flex-col ml-3">
+                <h4 className='font-poppins font-semibold text-white
+                text-[18px] leading-[23px] mb-1'>
+                    {title}
+                </h4>
+                <p className='font-poppins font-normal text-dimWhite
+                text-[16px] leading-[24px] mb-1'>
+                    {content}
+                </p>
+            </div>
+        </div>
+    );
+};
+
 const Business = () => {
     return (
         <section id="features" className={layout.section}>
@@ -19,6 +48,12 @@ const Business = () => {
                     cards on the market.
                 </p>
                 <Button styles="mt-10" />
+            </div>
+
+            <div className={`${layout.sectionImg} flex-col`}>
+                {features.map((feature, index) => (
+                    <FeatureCard key={feature.id} {...feature} index={index} />
+                ))}
             </div>
         </section>
     );
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ type PropTypes = {
 const Button = ({ styles }: PropTypes) => {
     return (
         <button type="button" className={`py-4 px-6
-        bg-blue-gradient font-poppins font-medium
+        bg-blue-gradient font-poppins font-medium rounded-[10px]
         text-[18px] text-primary outline-none ${styles}`}>
             Get Started
         </button>
